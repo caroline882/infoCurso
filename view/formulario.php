bee406763c8ab5d4346184882f785504a1271464
Caroline <tremarincaroline8@gmail.com>
2021-11-09 22:01:27 -0300

    estilo css

---
+++ view/formulario.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora!</title>
+    <link rel="stylesheet" type="text/css" href="./css/estilo.css">
+</head>
+<body>
+<form class="center" action="/calculadora.php">
+  <label for="numero">Calculadora online! </label><br>
+  <label for="numero"> </label><br>
+  <label for="numero">Digite um número:</label><br>
+  <input class="php" type="text" id="numero" name="numero" ><br>
+  <label for="numero"> </label><br>
+  <label class="carol" for="numero2">Digite outro número:</label><br>
+  <input class="php2" id="numero2" name="numero2" ><br><br>
+  <input type="submit" value="Calcular">
+  
+</form>
+</body>
+</html>
