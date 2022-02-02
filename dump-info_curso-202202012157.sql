6c18a16e246f8bba97ba6e823f88e04bfaa9fe7e
caroline <tremarincaroline8@gmail.com>
2022-02-01 22:00:24 -0300

    01-02

---
+++ dump-info_curso-202202012157.sql
@@ -0,0 +1,145 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: info_curso
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.3.31-MariaDB-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `clientes` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `sobrenome` varchar(150) NOT NULL,
+  `data_nascimento` date DEFAULT '0001-01-01',
+  `sexo` varchar(1) NOT NULL DEFAULT 'F',
+  `rg` varchar(10) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `cnpj` varchar(14) DEFAULT NULL,
+  `tipo` varchar(1) NOT NULL DEFAULT 'F',
+  `email` varchar(225) NOT NULL,
+  `telefone` varchar(10) NOT NULL,
+  `celular` varchar(11) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  `sigla_estado` varchar(2) NOT NULL,
+  `cidade` varchar(255) DEFAULT NULL,
+  `cep` varchar(8) NOT NULL,
+  `bairro` varchar(150) NOT NULL,
+  `logradouro` varchar(255) NOT NULL,
+  `numero` tinyint(4) DEFAULT 0,
+  `complemento` varchar(255) DEFAULT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `produtos` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `classificacao` varchar(30) DEFAULT NULL,
+  `quantidade` decimal(15,2) DEFAULT 0.00,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `nome` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `sku` varchar(150) NOT NULL DEFAULT '0',
+  `peso` decimal(15,4) NOT NULL DEFAULT 0.0000,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `largura` varchar(30) DEFAULT NULL,
+  `altura` varchar(30) DEFAULT NULL,
+  `profundidade` varchar(30) DEFAULT NULL,
+  `preco` decimal(15,4) NOT NULL DEFAULT 0.0000,
+  `preco_promocional` decimal(15,4) DEFAULT 0.0000,
+  `gtin` varchar(30) NOT NULL DEFAULT '0',
+  `unidade_medida` varchar(10) DEFAULT NULL,
+  `condicao` varchar(50) NOT NULL DEFAULT 'Novo',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuarios` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(150) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 1,
+  `email_recuperacao` varchar(255) NOT NULL,
+  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_atualizacao` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Caroline','caroline20','tremarincaroline8@gmail.com','qwe123',1,'tremarincaroline8@gmail.com','2022-02-02 00:01:21','2022-02-02 00:01:21');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'info_curso'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-01 21:57:33
